88ca1e629215b3a8a6f954367f43403611fb060d
panth1823 <panthshah242@gmail.com>
2024-02-12 22:53:36 +0530

    added custom cursor

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import Image from './components/Image'
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <Image/> */}
   </React.StrictMode>
 );
